83d19015592e59d5f7c933c405c4435d80d600a5
Pasindu Promod <dev@ninehermits.com>
2025-02-25 15:16:41 +0530

    first commit

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1740476768,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/redlightgreenlighthowto-sheet0.png",
+		"images/redlightgreenlighthowto-sheet1.png",
+		"images/play-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/happyface-sheet0.png",
+		"images/happyface-sheet1.png",
+		"images/usdc-sheet0.png",
+		"images/usdc-sheet1.png",
+		"images/sprite-sheet0.png",
+		"media/tap.m4a",
+		"media/tap.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
